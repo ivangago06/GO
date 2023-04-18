463dc80d834e8110873c9eb9cc047d3ff43fb284
Gerardo Ivan Garcia Gonzalez <gerardo.garcia@ipbtel.local>
2023-04-18 10:50:41 -0600

    Tarea Update: 2 ejemplos de errores.

--- 08-manejo-de-errores/main.go
+++ 08-manejo-de-errores/main.go
@@ -2,10 +2,43 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
+
+type ErrorPersonalizado struct {
+	mensaje string
+}
 
 func main() {
 
-	fmt.Println("Hacer 2 ejemplos de tarea para cachar el error, para el martes 18 de abril")
+	// Llamada a la función multiplicar
+	resultado1 := multiplicar(47, 0)
+	fmt.Println("El resultado de la multiplicación es:", resultado1)
 
+	// Llamada a la función potencia
+	resultado2, err := potencia(0, 0)
+	if err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Println("El resultado de la potencia es:", resultado2)
+	}
+
+}
+
+func multiplicar(a, b float64) float64 {
+	if b == 0 || a == 0 {
+		panic("Cualquier número multiplicado por cero da como producto cero. Esto se conoce como la propiedad cero de la multiplicación.")
+	}
+	return a * b
+}
+
+func (e *ErrorPersonalizado) Error() string {
+	return e.mensaje
+}
+
+func potencia(a, b float64) (float64, error) {
+	if b == 0 {
+		return 0, &ErrorPersonalizado{"Si la base y el exponente son 0 s considera como indeterminado"}
+	}
+	return math.Pow(a, b), nil
 }
